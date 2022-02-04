315e509a661d26e53b616b7795613c5d4c5d4379
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-02-04 19:27:26 +0800

    Implement auth service

--- auth-service/src/routes/current-user.ts
+++ auth-service/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { currentUser } from '@trading-jutsu/common';
 
 const router = express.Router();
 
-router.get('/api/users/current-user', (req, res) => {
-  res.send({ id: '1', email: 'pmofrancisco@yahoo.com' });
+router.get('/api/users/current-user', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
