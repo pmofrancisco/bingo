01648b7c3f939c8221b03f906b55ecb5b1ca82b9
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-02-04 15:33:31 +0800

    Added MongoDB

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,6 +1,18 @@
+import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
   app.listen(3000, () => {
     console.log('Listening on port 3000');
   });
