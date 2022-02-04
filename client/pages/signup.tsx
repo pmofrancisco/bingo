091b8cbd3e4113a460a951430d90b42f22d878e8
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-02-04 13:15:45 +0800

    Call current user api

--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -1,5 +1,14 @@
+import axios from 'axios';
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
 
-const Signup: NextPage = () => (<div>Signup Page</div>);
+const Signup: NextPage = () => {
+  useEffect(() => {
+    axios.get('/api/users/current-user').then((res) => {
+      console.log('res.data', res.data);
+    });
+  }, []);
+  return (<div>Signup Page</div>);
+};
 
 export default Signup;
