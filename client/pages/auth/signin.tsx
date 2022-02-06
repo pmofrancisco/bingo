e6e8bfb6ffffb6d9b054b5adc05ae6837998bf2f
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-02-06 19:22:49 +0800

    Created signin page

---
+++ client/pages/auth/signin.tsx
@@ -0,0 +1,54 @@
+import type { NextPage } from 'next';
+import * as R from 'ramda';
+import { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import { PrimaryButton, TextField } from '@fluentui/react';
+
+const mutation = gql`
+  mutation Signin($email: String, $password: String) {
+    signin(email: $email, password: $password) {
+      id
+      email
+    }
+  }
+`;
+
+type FormValuesType = {
+  email: string;
+  password: string;
+};
+
+const Signup: NextPage = () => {
+  const [signup] = useMutation(mutation);
+  const [formValues, setFormValues] = useState<FormValuesType>({
+    email: '',
+    password: '',
+  });
+
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      const { email, password } = formValues;
+      signup({
+        variables: {
+          email,
+          password,
+        }
+      });
+    }}>
+      <TextField
+        label="Email"
+        onChange={(e, newValue) => setFormValues(R.assoc('email', newValue || '', formValues))}
+      />
+      <TextField
+        canRevealPassword
+        label="Password"
+        type="password"
+        onChange={(e, newValue) => setFormValues(R.assoc('password', newValue || '', formValues))}
+      />
+      <PrimaryButton text="Signin" type="submit" />
+    </form>
+  );
+};
+
+export default Signup;
