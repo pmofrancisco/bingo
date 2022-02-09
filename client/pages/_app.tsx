0e895fcd256b742ec2f87705ed07537a6cd888ff
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-02-09 14:56:27 +0800

    Initialize icons on useEffect

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,16 +1,19 @@
+import { useEffect } from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { initializeIcons } from '@fluentui/font-icons-mdl2';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
-
-initializeIcons();
-
 function MyApp({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  useEffect(() => {
+    initializeIcons();
+  }, []);
+
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
